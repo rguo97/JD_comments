a85e3befe21099ed7c0fb532b3128cf595fc0e00
rguo97 <73048036+rguo97@users.noreply.github.com>
2021-02-24 09:59:08 +0800

    Create jd.py

---
+++ jd_commodity/jd.py
@@ -0,0 +1,12 @@
+from common.database import Database
+
+class Jd(Database):
+    def __init__(self, database):
+        super().__init__(database)
+
+
+    def get_row_num(self):
+        pass
+
+
+
